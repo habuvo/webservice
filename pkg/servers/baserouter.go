734e36f8bc8c56f96fe9ccbd0c8603f0fe01abf1
Ilya Glukhov <ilya@canto.ru>
2021-07-05 11:15:26 +0300

    adds healthcheck and readness probe

--- pkg/servers/baserouter.go
+++ pkg/servers/baserouter.go
@@ -2,11 +2,21 @@ package servers
 
 import (
 	"net/http"
+	"sync/atomic"
+	"time"
 
 	"github.com/habuvo/webservice/pkg/handlers"
 )
 
 func NewBaseRouter(storage handlers.Storage) (router *http.ServeMux) {
+	isReady := &atomic.Value{}
+	isReady.Store(false)
+	go func() {
+		//some useness checks
+		time.Sleep(10 * time.Second)
+		isReady.Store(true)
+	}()
+
 	hb := handlers.NewBundle(storage)
 
 	r := http.NewServeMux()
@@ -14,6 +24,24 @@ func NewBaseRouter(storage handlers.Storage) (router *http.ServeMux) {
 	// OPTIONS
 	// r.Handle("/*", http.HandlerFunc(handlers.Preflight))
 	r.Handle("/create", handlers.CORSHandler(http.HandlerFunc(hb.Create)))
+	r.HandleFunc("/healthz", healthz)
+	r.HandleFunc("/readyz", readyz(isReady))
 
 	return
+}
+
+// healthz is a liveness probe.
+func healthz(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+// readyz is a readiness probe.
+func readyz(isReady *atomic.Value) http.HandlerFunc {
+	return func(w http.ResponseWriter, _ *http.Request) {
+		if isReady == nil || !isReady.Load().(bool) {
+			http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}
 }
